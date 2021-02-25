f28a05356516d8519696e9cdceb75e8e879957de
Yannick <yannick.aiguier@gmail.com>
2021-02-25 15:23:16 +0100

    fix: catalog.blade.php was gone

---
+++ resources/views/catalog.blade.php
@@ -0,0 +1,6 @@
+<h1>Tout notre choix de maté</h1>
+<?php foreach ($products as $product) { ?>
+<h2><a href="/product/<?=$product->id?>"><?=$product->title?></a></h2>
+<p><?=$product->description?></p>
+<hr>
+<?php } ?>
