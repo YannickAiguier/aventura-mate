7a341dff4a7aca04878e65fcbd27f89e1579b7f0
Yannick <yannick.aiguier@gmail.com>
2021-02-25 14:35:28 +0100

    feat: simple catalog view completed (route, view and controller) with link to pruduct page #7

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Notre catalogue complet</title>
-</head>
-<body>
-<h1>Tout notre choix de maté</h1>
-<?php foreach ($products as $product) { ?>
-<h2><?=$product->title?></h2>
-<p><?=$product->description?></p>
-<hr>
-<?php } ?>
-</body>
-</html>
+@extends('base')
+
+@section('content')
+    <h1>Tout notre choix de maté</h1>
+    <?php foreach ($products as $product) { ?>
+    <h2><a href="/product/<?=$product->id?>"><?=$product->title?></a></h2>
+    <p><?=$product->description?></p>
+    <hr>
+    <?php } ?>
+@endsection
