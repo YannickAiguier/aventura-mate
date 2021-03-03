a482a4dd3e7330897a36666c3005d2e1da86dd51
Anthony-Perriolat <75612701+Anthony-Perriolat@users.noreply.github.com>
2021-03-03 10:48:04 +0100

    change view for redirect

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -86,7 +86,7 @@ class CartController extends Controller
 
         }
         session(['cart' => $tab]);
-        return view('cart');
+        return redirect()->action([CartController::class, 'index']);
     }
 
     /**
