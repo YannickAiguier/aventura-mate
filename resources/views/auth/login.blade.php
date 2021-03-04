6217e7b5a72b2dced694aa4d36ada0b6741e466f
Yannick <yannick.aiguier@gmail.com>
2021-03-04 16:08:14 +0100

    feat: register link on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,9 +13,9 @@
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('login') }}">
-            @csrf
+        @csrf
 
-            <!-- Email Address -->
+        <!-- Email Address -->
             <div>
                 <x-label for="email" :value="__('Email')" />
 
@@ -27,9 +27,9 @@
                 <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                         type="password"
+                         name="password"
+                         required autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
@@ -38,6 +38,11 @@
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
+            </div>
+
+            <!-- Register -->
+            <div>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{route('register')}}">{{__('Register')}}</a>
             </div>
 
             <div class="flex items-center justify-end mt-4">
